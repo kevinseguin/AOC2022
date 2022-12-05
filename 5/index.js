8dac57900f22b19e2a813c1c229031556dadd762
Kevin Seguin <kevinseguin@gmail.com>
2022-12-05 11:22:26 -0500

    cleanup 5

--- 5/index.js
+++ 5/index.js
@@ -4,45 +4,42 @@ const data = fs.readFileSync('input.txt', 'utf-8');
 const lines = data.replace(/\r/g, '').split("\n")
 
 var stacks = new Array(9);
-stacks.fill('')
+stacks.fill('') 
 
-console.clear()
+resetStacks();
 
-for (i = 0; i < 8 ; i++ ) { 
-    var thisLine = lines[i];  
-    var idx =0;
-    _.times(9, (n) => { 
-        idx += n == 0 ? 1 : 4;
-        var stackValue = thisLine[idx] 
-        stacks[n] = stacks[n] += stackValue.replace(' ', '')
-    } )         
-    
-}
+
 
 // //Part 1
-// console.table(stacks)
+console.log('===PART 1===')
+console.log('===START===')
 
-// for (l = 10 ; l < lines.length ; l++ ) {
-//     var numparts = lines[l].split(' ');
-//     moveCrate9000(numparts[1], parseInt(numparts[3])-1, parseInt(numparts[5])-1)
-//     console.table(stacks);
-// }
+console.table(stacks)
 
+
+for (l = 10 ; l < lines.length ; l++ ) {
+    var numparts = lines[l].split(' ');
+    moveCrate9000(numparts[1], parseInt(numparts[3])-1, parseInt(numparts[5])-1)
+   
+}
+console.log('===END===')
+console.table(stacks);
 
 //Part 2
- console.log('part2')
- console.table(stacks)
+resetStacks()
+console.log('PART 2')
+console.log('===START===')
+console.table(stacks)
 
 for (l = 10 ; l < lines.length ; l++ ) {
     var numparts = lines[l].split(' ');
     moveCrate9001(numparts[1], parseInt(numparts[3])-1, parseInt(numparts[5])-1)
-    console.table(stacks);
+
 }
+console.log('===END===')
+console.table(stacks);
 
-
-
-
-
+ 
 
 function moveCrate9000(num, from, to) {
     var cratesToMove = new Array();
@@ -53,9 +50,6 @@ function moveCrate9000(num, from, to) {
     });
 
     var stringToMove = cratesToMove.toString().replaceAll(',','');
-
-    console.log(`move ${stringToMove} from ${from+1} to ${to+1}`)
- 
     stacks[to] = stringToMove + stacks[to];
  
 }
@@ -69,10 +63,23 @@ function moveCrate9001(num, from, to) {
     });
 
     var stringToMove = cratesToMove.toString().replaceAll(',','');
-
-    console.log(`move ${stringToMove} from ${from+1} to ${to+1}`)
- 
     stacks[to] = stringToMove + stacks[to];
  
 }
 
+
+function resetStacks() {
+stacks = new Array(9);
+stacks.fill('')
+
+for (i = 0; i < 8 ; i++ ) { 
+    var thisLine = lines[i];  
+    var idx =0;
+    _.times(9, (n) => { 
+        idx += n == 0 ? 1 : 4;
+        var stackValue = thisLine[idx] 
+        stacks[n] = stacks[n] += stackValue.replace(' ', '')
+    } )         
+    
+}
+}
