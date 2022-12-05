06d9d3650845ce1d5231a017f7c2e6a1350c545c
Kevin Seguin <kevinseguin@gmail.com>
2022-12-05 16:21:08 -0500

    refactoring for change of input

--- 5/index.js
+++ 5/index.js
@@ -1,14 +1,30 @@
 const _ = require('underscore')
 const fs = require('fs');
+const { createInflate } = require('zlib');
 const data = fs.readFileSync('input.txt', 'utf-8');
 const lines = data.replace(/\r/g, '').split("\n")
 
-var stacks = new Array(9);
+var crateHeight = 0;
+var numStacks = 0;
+var instructionsStartLine = 0;
+
+_.forEach(lines, (l,idx)=> { 
+    //a crate line
+    if (l[0] == '[') { crateHeight++; }
+
+    //the count of stacks line
+    if (l[1] == 1) {
+        numStacks = parseInt(_.last(_.filter(l.split(' '), d=> { return parseInt(d)} )))
+    }
+
+    if (l.startsWith('move') && instructionsStartLine == 0) instructionsStartLine = idx;
+
+})
+
+var stacks = new Array(numStacks)
 stacks.fill('') 
 
-resetStacks();
-
-
+resetStacks(); 
 
 // //Part 1
 console.log('===PART 1===')
@@ -17,7 +33,7 @@ console.log('===START===')
 console.table(stacks)
 
 
-for (l = 10 ; l < lines.length ; l++ ) {
+for (l = instructionsStartLine ; l < lines.length ; l++ ) {
     var numparts = lines[l].split(' ');
     moveCrate9000(numparts[1], parseInt(numparts[3])-1, parseInt(numparts[5])-1)
    
@@ -31,7 +47,7 @@ console.log('PART 2')
 console.log('===START===')
 console.table(stacks)
 
-for (l = 10 ; l < lines.length ; l++ ) {
+for (l = instructionsStartLine ; l < lines.length ; l++ ) {
     var numparts = lines[l].split(' ');
     moveCrate9001(numparts[1], parseInt(numparts[3])-1, parseInt(numparts[5])-1)
 
@@ -69,17 +85,17 @@ function moveCrate9001(num, from, to) {
 
 
 function resetStacks() {
-stacks = new Array(9);
-stacks.fill('')
+    stacks = new Array(numStacks);
+    stacks.fill('')
 
-for (i = 0; i < 8 ; i++ ) { 
-    var thisLine = lines[i];  
-    var idx =0;
-    _.times(9, (n) => { 
-        idx += n == 0 ? 1 : 4;
-        var stackValue = thisLine[idx] 
-        stacks[n] = stacks[n] += stackValue.replace(' ', '')
-    } )         
-    
-}
+    for (i = 0; i < crateHeight ; i++ ) { 
+        var thisLine = lines[i];  
+        var idx =0;
+        _.times(numStacks, (n) => { 
+            idx += n == 0 ? 1 : 4;
+            var stackValue = thisLine[idx] 
+            stacks[n] = stacks[n] += stackValue.replace(' ', '')
+        } )         
+        
+    }
 }
